a6916ebdc4e3f3fd7a90a8b12af729798182f550
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-04-15 22:09:42 -0300

    primeiro commit do projeto

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <header>
+        <div class="container">
+           <div class="header-content">
+                <img src="/src/assets/pokelogo.svg" alt="">
+                <ul>
+                    <li><a href="#">Português BR</a></li>
+                    <li><a href="#">Espanhol</a></li>
+                    <li><a href="#">Inglês</a></li>
+                </ul>
+           </div>
+        </div>
+    </header>
+</template>
+
+
+<script>
+    
+</script>
+
+<style>
+    header{
+        width: 100%;
+        background-color: #4B4B4D;
+    }
+
+    .header-content{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 12px 0;
+    }
+
+    .header-content ul{
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+    }
+
+    .header-content ul li a{
+        color: white;
+        font-size: .875rem;
+    }
+
+</style>
